e46903113d988ad715d18538cd6d85b21e22d91e
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-05-15 01:47:45 +0300

    retry

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from "../features/loginState/LoginSlice.js";
-
-
-export const store = configureStore({
-  reducer: {
-    login: loginReducer,
-  },
-});
